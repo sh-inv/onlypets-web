221e4a7df658baee3e5f939f44fdecf9200ef0c9
youbin <you8inpark@gmail.com>
2023-03-19 01:09:52 +0900

    Feature: 메인 스크린 레이아웃

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import Header from "@/layout/Header";
-import GlobalStyle from "@/styles/GlobalStyle";
-import { theme } from "@/styles/theme";
-import type { AppProps } from "next/app";
-import styled, { ThemeProvider } from "styled-components";
+import Header from '@/layout/Header';
+import GlobalStyle from '@/styles/GlobalStyle';
+import { theme } from '@/styles/theme';
+import type { AppProps } from 'next/app';
+import styled, { ThemeProvider } from 'styled-components';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -17,7 +17,7 @@ const App = ({ Component, pageProps }: AppProps) => {
 };
 
 const DefaltWidth = styled.div`
-  width: 1140px;
+  /* width: 1140px; */
   margin: 0 auto;
 `;
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,49 @@
+import styled from 'styled-components';
+import Image from 'next/image';
+import main from '../assets/main.png';
+
 const Home = () => {
-  return;
+  return (
+    <Positioner>
+      <MainScreen>
+        <div className='image-wrapper'>
+          <Image alt='main' src={main} className='image' />
+        </div>
+        <div className='background' />
+        {/* <div className='content-wrapper'></div> */}
+      </MainScreen>
+    </Positioner>
+  );
 };
+
+const Positioner = styled.section`
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+`;
+
+const MainScreen = styled.div`
+  position: relative;
+
+  .image-wrapper {
+    width: auto;
+    height: 100vh;
+
+    .image {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+
+  .background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 500px;
+    background: linear-gradient(180deg, white 0%, white 59px, rgba(255, 255, 255, 0) 100%);
+  }
+`;
 
 export default Home;
